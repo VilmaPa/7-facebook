e36f52c58bb9d4aa0bbca0a3e94365c0c05a1b1e
VilmaPa <vilma.papeikiene@gmail.com>
2020-10-05 15:53:01 +0300

    img part

--- js/renderContentGallery.js
+++ js/renderContentGallery.js
@@ -1,10 +1,35 @@
-//import renderContent from "./renderContent";
+function renderContentGallery(photoArray) {
+    // validacijos
+    // jei nera duotas nuotrauku sarasas
+    if (!photoArray) {
+        return '';
+    }
 
-function renderContentGallery() {
-    return `<div class="gallery">
-        IMAGES
+    // logika
+    const maxGallerySize = 4;
+    const printablePhotosCount = photoArray.length > maxGallerySize ? maxGallerySize : photoArray.length;
+
+    // generuojame tik reikiama kieki nuotrauku
+    let extraHTML = '';
+    if (photoArray.length > maxGallerySize) {
+        extraHTML = `data-extra="${photoArray.length - maxGallerySize}"`;
+    }
+
+    let photosHTML = '';
+    for (let i = 0; i < printablePhotosCount; i++) {
+        if (i < printablePhotosCount - 1) {
+            photosHTML += `<img src="./img/posts/${photoArray[i]}"
+                                alt="User post gallery picture">`;
+        } else {
+            photosHTML += `<img src="./img/posts/${photoArray[i]}"
+                                alt="User post gallery picture"
+                                ${extraHTML}>`;
+        }
+    }
+
+    return `<div class="gallery gallery-${printablePhotosCount}">
+                ${photosHTML}
             </div>`;
 }
-
 
 export default renderContentGallery;
